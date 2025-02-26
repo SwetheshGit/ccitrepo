24904e9fbba1ba465c89fbda580b96ffbadaa0ef
SwetheshGit <swetheshmuddina2001@gmail.com>
2025-02-26 15:01:43 +0530

    Update CloudInfra.tf

--- CloudInfra.tf
+++ CloudInfra.tf
@@ -1,9 +1,9 @@
-provider "aws"{
-region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-2"  # Updated AWS region
 }
 
-#Create an S3 bucket
+# Create an S3 bucket
 resource "aws_s3_bucket" "ccit_s3" {
   bucket = "ccit-public-bucket"
-  tags = { Name = "ccitbucket1" }
+  tags = { Name = "ccit-public-bucket" }
 }
