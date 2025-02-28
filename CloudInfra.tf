f3895dc62e919ba86842403923b01e00c9bc3345
SwetheshGit <swetheshmuddina2001@gmail.com>
2025-02-28 09:46:54 +0530

    Update CloudInfra.tf

--- CloudInfra.tf
+++ CloudInfra.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "ccit_s3" {
   tags = { Name = "ccit-public-bucket" }
 }
 
-resource "aws_s3_bucket" "ccit_s3" {
+resource "aws_s3_bucket" "cc_s3" {
   bucket = "ccit-public-bucket"
   tags = { Name = "ccit s3" }
 }
